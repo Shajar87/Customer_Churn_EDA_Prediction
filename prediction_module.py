5675297a832c4eaacd19bec04c23117699dbbf50
Shajar87 <shameem.shajar@gmail.com>
2024-01-13 20:27:44 +0530

    Added all remaining files

---
+++ prediction_module.py
@@ -0,0 +1,22 @@
+import pickle
+import traceback  
+
+
+def make_prediction(df):
+    try:
+
+        #Fill missing values
+        df.ffill(inplace=True)
+        # Load the model
+        with open(r'model', 'rb') as file:
+            loaded_model = pickle.load(file)
+
+        # Make predictions
+        
+        rf_prediction = loaded_model.predict(df)
+        return rf_prediction
+    except Exception as e:
+        print(f"Error making prediction: {e}")
+        traceback.print_exc()
+        return None
+ 
